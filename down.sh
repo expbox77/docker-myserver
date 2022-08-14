53302f32f3736c8fb025504ebfe65070d036f8f8
expbox77 <expbox77@gmail.com>
2022-08-15 00:02:03 +0900

    일부 수정

--- down.sh
+++ down.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# 도커 컴포즈 종료
 
 # 버전 정보 가져오기
 source .version
@@ -13,10 +14,15 @@ if [[ -f "$Secret_File" ]]; then
     source $Secret_File
     
 else
-    echo $Secret_File "file could not be found." 
-    echo "Exit the task. Error Code 101"
+    echo -e "\e[43;31mERROR!\e[0m"
+    echo "\$Secret_File file could not be found." 
+    echo -e "Exit the task. Error Code \e[43;31m101\e[0m"
     exit 101
 fi
+
+# 종료 메시지
+echo "##### Start Docker-Compose Shutdown Process #####"
+echo ""
 
 # 각 폴더명 추출
 for entry in */
@@ -28,6 +34,9 @@ do
     docker-compose -f ./${entry:0:-1}/docker-compose.yml down
 done
 
+echo "##### Finish Docker-Compose Shutdown Process #####"
+echo ""
+
 # 네트워크 삭제
 echo ""
 echo "##### Start Deleting External Network #####"
@@ -36,12 +45,12 @@ for check_net in ${docker_net[@]}
 do
     if [[ "$(docker network ls)" == *$check_net* ]]; then
         # 있을 때 삭제
-        echo "Deleting [" $check_net "] network."
+        echo "Deleting [ $check_net ] network."
         docker network rm $check_net > /dev/null
         echo "Done!"
 
     else
-        echo "Could not find [" $check_net "] network."
+        echo "Could not find [ $check_net ] network."
     fi
 done
 
